ecfc04ed170aad140261547a385b012149a9c2d4
mattayoungs <mattayoungs@icloud.com>
2020-05-15 15:50:45 -0400

    started early jsx and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import add from "./icon/mdi_add.svg";
+import list from "./icon/mdi_view_list.svg";
+import timer from "./icon/mdi_timelapse.svg";
+import settings from "./icon/mdi_settings.svg";
+import arrow from "./icon/mdi_keyboard_arrow_down.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="navbar">
+        <img src={list} alt="" />
+        <img src={timer} alt="" />
+        <img src={settings} alt="" />
       </header>
-    </div>
+      <h1 className="title">To Do App</h1>
+      <div className="todo-cont">
+        <div className="task">
+          <input className="task__checkbox" type="checkbox" name="" id="" />
+          <label className="task__title" for="">
+            Get Groceries
+          </label>
+          <img src={arrow} alt="" />
+        </div>
+        <div className="task">
+          <input className="task__checkbox" type="checkbox" name="" id="" />
+          <label className="task__title" for="">
+            Get Groceries
+          </label>
+          <img src={arrow} alt="" />
+        </div>
+        <div className="task">
+          <input className="task__checkbox" type="checkbox" name="" id="" />
+          <label className="task__title" for="">
+            Get Groceries
+          </label>
+          <img src={arrow} alt="" />
+        </div>
+      </div>
+
+      <button className="add-task-fab">
+        <img src={add} alt="Add Task" />{" "}
+      </button>
+    </>
   );
 }
 
